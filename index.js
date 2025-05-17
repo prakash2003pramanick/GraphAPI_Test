508f72f48e13158ebf18d875a9f3060042c5ee37
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2025-05-17 15:16:43 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const code = req.query?.code;
+
+  // Print incoming URL
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log("Incoming URL:", fullUrl);
+
+  // Debug: query and body
+  console.log("req.query", req.query);
+  console.log("req.body", req.body);
+  console.log("Authorization code:", code);
+
+  res.send('Code received. You can now use it in Postman.');
+});
+ 
+
+app.listen(3000, () => console.log('Listening on http://localhost:3000'));
